230392b5d162956c31d2fdbbc33afeb2789fa06d
Tim Skau <timskau1975@gmail.com>
2017-04-30 22:50:06 +0200

    o

--- routes/hello.js
+++ routes/hello.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var path = __dirname + '/views/';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   //.render('index', { title: 'hello there' });
-  res.send('respond with a resource');
+  //res.send('Hello world');
+  res.sendFile(path + "hello.html");
   
 });
 
